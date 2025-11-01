9576443127d8252de3716ae081e8582ffa588d11
matin-dev1 <matindev007@gmail.com>
2025-11-01 19:52:01 +0330

    stopwatch-mimi-project

--- src/Timer.js
+++ src/Timer.js
@@ -8,35 +8,70 @@ class Timer extends React.Component {
   constructor(){
     super();
     this.state = {
-      time : 10
+      hour : 0,
+      minute:0,
+      second:0,
+      isStart:false
     }
   }
 
-componentDidMount(){
-  interval = setInterval(()=>{
+  startInterval = ()=>{
+    if(this.state.isStart == false){
+      this.setState({
+        isStart : true
+      })
+      interval = setInterval(()=>{
+        this.setState({
+          second: this.state.second + 1        
+        })
+        if(this.state.second == 60){
+          this.setState({
+            second:0,
+            minute: this.state.minute + 1
+          })
+        }
+        if(this.state.minute == 60){
+          this.setState({
+            minute:0,
+            hour: this.state.hour + 1
+          })
+        }      
+      } , 1000)      
+    }
+  }
+
+  stopInterval = ()=>{
     this.setState({
-      time:this.state.time - 1        
+      isStart : false
     })
-  },1000)
-}
+    clearInterval(interval);
+  }
 
-componentDidUpdate(){
-  if (this.state.time === 0){
-    clearInterval(interval)
-  } 
-}
+  resetInterval = ()=>{
+    this.stopInterval();
+    this.setState({
+      hour:0,
+      minute:0,
+      second:0
+    })
 
-componentWillUnmount(){
+  }
 
-}
 
- render(){
+  render(){
+    let h = this.state.hour
+    let m = this.state.minute
+    let s = this.state.second
     return (
       <>
       <h2 className='timer'>
-        it is {this.state.time}
+        {`${h > 9 ? h : "0"+h} : ${m > 9 ? m : "0"+m} : ${s > 9 ? s : "0"+s}`}
       </h2>
-      <button onClick={this.props.handleSetTtitle}>change</button>
+      <div className = "button_box">
+        <span className="action_button start_button" onClick={this.startInterval}>start</span>
+        <span className="action_button stop_button" onClick={this.stopInterval}>stop</span>
+        <span className="action_button reset_button" onClick={this.resetInterval}>reset</span>
+      </div>
       </>
     )
   }

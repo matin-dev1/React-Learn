73c6f15bf1e4b119b433d16eab1e11d84d40bbe9
matin-dev1 <matindev007@gmail.com>
2025-11-18 11:18:49 +0330

    add-users

--- src/users/Users.jsx
+++ src/users/Users.jsx
@@ -1,10 +1,58 @@
-import React from 'react';
-import style from '../style.module.css'
-import { Link, useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link , useNavigate} from 'react-router-dom';
+import style from '../style.module.css';
+import swal from 'sweetalert';
+import axios from 'axios';
 
 const Users = ()=>{
 
-    const navigate = useNavigate()
+    const navigate = useNavigate();
+    const [users , setUsers] = useState([]);
+
+    useEffect(() => {
+        axios.get('https://jsonplaceholder.typicode.com/users').then(res=>{
+            setUsers(res.data);
+        }).catch(err=>{
+            console.log(err);
+        })
+    }, []);
+
+    const handleDelete = (itemId)=>{
+        swal({
+            title: "حذف رکورد !",
+            text: `آیا از حذف رکورد ${itemId} اطمینان دارید؟`,
+            icon: "warning",
+            buttons: ["خیر" , "بله"],
+            dangerMode: true,
+          })
+          .then((willDelete) => {
+            if (willDelete) {
+
+                axios({
+                    method:"DELETE",
+                    url:`https://jsonplaceholder.typicode.com/users/${itemId}`
+                }).then(res=>{
+
+                    if (res.status === 200){
+                        const newUsers = users.filter(u=>u.id != itemId);
+                        setUsers(newUsers);
+                        swal("حذف با موفقیت انجام شد", {
+                            icon: "success",
+                            buttons: "متوجه شدم",
+                        });
+                    }else{
+                        swal("عملیات با خطا مواجه شد",{
+                            icon: "error",
+                            button:"متوجه شدم"
+                        });
+                    }
+                })
+
+            } else {
+              swal("شما از حذف رکورد منصرف شدید");
+            }
+          });
+    }
 
     return (
         <div className={`${style.item_content} mt-5 p-4 container-fluid`}>
@@ -14,13 +62,14 @@ const Users = ()=>{
                     <input type="text" className="form-control shadow" placeholder="جستجو"/>
                 </div>
                 <div className="col-2 text-start px-0">
-                    <Link to='/user/add'>
+                    <Link to="/user/add" state={"vue"}>
                         <button className="btn btn-success">
                             <i className="fas fa-plus text-light"></i>
                         </button>
                     </Link>
                 </div>
             </div>
+            {users.length ? (
             <table className="table bg-light shadow">
                 <thead>
                     <tr>
@@ -32,20 +81,34 @@ const Users = ()=>{
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>matin</td>
-                        <td>matinB</td>
-                        <td>matnbld@gmail.com</td>
+                   {users.map(u => (
+                    <tr key={u.id}>
+                        <td>{u.id}</td>
+                        <td>{u.name}</td>
+                        <td>{u.username}</td>
+                        <td>{u.email}</td>
                         <td>
                             <i className="fas fa-edit text-warning mx-2 pointer"
-                            onClick={()=>navigate("/user/add/2")}></i>
-                            
-                            <i className="fas fa-trash text-danger mx-2 pointer"></i>
+                            onClick={()=>navigate("/user/add/2" , {
+                                state : 
+                                    {
+                                        x: "react",
+                                        y:"angular"
+                                    }
+                                })}
+                            ></i>
+                            <i className="fas fa-trash text-danger mx-2 pointer"
+                            onClick={()=>handleDelete(u.id)}
+                            ></i>
                         </td>
                     </tr>
+                   ))}
                 </tbody>
             </table>
+            ) : (
+                <h4 className="text-center text-info">لطفا صبر کنید...</h4>
+            )}
+
         </div>
     )
 

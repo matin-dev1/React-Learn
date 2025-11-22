0a9c61faf8852a0019c2be3c0b3bdc7f47bb0e9d
matin-dev1 <matindev007@gmail.com>
2025-11-22 12:16:33 +0330

    HOC-part2

--- src/posts/ClickCount.jsx
+++ src/posts/ClickCount.jsx
@@ -2,12 +2,12 @@ import React, { useState } from 'react';
 import Counter from './Counter';
 
 const ClickCount = (props)=>{
-    const {count , handleIncreaseCount} = props
+    const {count , handleIncreaseCount , name} = props
     return(
         <div className="text-center w-100">
-            <button className="btn btn-success" onClick={handleIncreaseCount}>count : {count}</button>
+            <button className="btn btn-success" onClick={handleIncreaseCount}> {name} count : {count}</button>
         </div>
     )
 }
 
-export default Counter(ClickCount);
+export default Counter(ClickCount , 3);
--- src/posts/HoverCount.jsx
+++ src/posts/HoverCount.jsx
@@ -13,4 +13,4 @@ const HoverCount = (props)=>{
     )
 }
 
-export default Counter(HoverCount);
+export default Counter(HoverCount , 5);
